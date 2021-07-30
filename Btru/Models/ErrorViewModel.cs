f5f2ca5eaa2c66d072d32c049ead8c8947141cc2
fghj <mitko.netzov@gmail.com>
2021-07-30 11:51:15 +0300

    initial ommit

---
+++ Btru/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Btru.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
